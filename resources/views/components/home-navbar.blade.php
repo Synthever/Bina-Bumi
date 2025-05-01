956d85624752bf75088adb93ae23a1197896d26e
Synthever <rakha@students.amikom.ac.id>
2025-05-01 17:19:36 +0700

    ok

--- resources/views/components/home-navbar.blade.php
+++ resources/views/components/home-navbar.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,44 +13,9 @@
     </style>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="font-sans max-w-lg mx-auto">
-    <script src="/js/sweetalert.js"></script>
-    <script>
-        @if (session('postsuccess'))
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.onmouseenter = Swal.stopTimer;
-                    toast.onmouseleave = Swal.resumeTimer;
-                }
-            });
-            Toast.fire({
-                icon: "success",
-                title: "{{ session('postsuccess') }}"
-            });
-        @endif
 
-        @if (session('postfailed'))
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.onmouseenter = Swal.stopTimer;
-                    toast.onmouseleave = Swal.resumeTimer;
-                }
-            });
-            Toast.fire({
-                icon: "error",
-                title: "{{ session('postfailed') }}"
-            });
-        @endif
-    </script>
+<body class="font-sans max-w-lg mx-auto w-800 --bg-black">
+
 </body>
+
 </html>
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -59,6 +59,45 @@
     </div>
 </div>
 
+<script src="/js/sweetalert.js"></script>
+<script>
+    @if (session('postsuccess'))
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "success",
+            title: "{{ session('postsuccess') }}"
+        });
+    @endif
+
+    @if (session('postfailed'))
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "error",
+            title: "{{ session('postfailed') }}"
+        });
+    @endif
+</script>
+
 
 
 <x-home-navbar />
