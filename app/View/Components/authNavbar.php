7999aec8b5851cccb349082622234f21d9762488
Synthever <rakha@students.amikom.ac.id>
2025-05-01 12:07:33 +0700

    Do Absolutely Nothing

--- app/View/Components/authNavbar.php
+++ app/View/Components/authNavbar.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class auth-navbar extends Component
+class authNavbar extends Component
 {
     /**
      * Create a new component instance.
