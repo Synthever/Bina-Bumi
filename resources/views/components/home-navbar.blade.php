ba604c84bbacebcf8fe5d60a0d84d447c93617a7
Synthever <rakha@students.amikom.ac.id>
2025-05-02 21:10:05 +0700

    update login register

--- resources/views/components/home-navbar.blade.php
+++ resources/views/components/home-navbar.blade.php
@@ -1,17 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beranda</title>
+    <title>Dashboard</title>
+    <link rel="icon" href="{{ asset('css/all/logo.png') }}" type="image/x-icon">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <style>
-        body {
-            font-family: 'Poppins';
-        }
-    </style>
+    <link href="{{ asset('css/home/style.css') }}" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="max-w-lg mx-auto w-800 --bg-black">
+<body class="w-full mx-auto">
